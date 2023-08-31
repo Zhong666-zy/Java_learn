33c84bdba852c3361d62aac0021877145b0a0142
zhongzy <1151434646@qq.com>
2023-08-31 20:52:57 +0800

    0831重新学习git

---
+++ untitled/learning/src/com/one/one_pl.java
@@ -0,0 +1,12 @@
+package com.one;
+
+public class one_pl {
+    public static void main(String[] args){
+        System.out.println("学学学");
+    }
+}
+// 项目上传到git上
+// 1. 首先file-setting-版本控制  git选择git.exe github登录你自己的账号
+// 2.1 创建本地git仓库
+// vcs-create git
+//
